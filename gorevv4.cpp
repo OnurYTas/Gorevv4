5c9a717ca990967c23b7564cffbdbecb2b1a7fc9
OnurYTas <71443413+OnurYTas@users.noreply.github.com>
2022-12-18 23:21:53 +0300

    Update gorevv4.cpp

--- gorevv4.cpp
+++ gorevv4.cpp
@@ -7,11 +7,11 @@ using namespace std;
 // Define a struct to represent a point in 2D space
 struct Country {
     string name;
-    string Capital;
+    string Capital;// Bunun da mı pointer olması gerekiyor?
 };
 struct Province {
     string name;
-    Country* country;
+    Country* country;// Domain gerekli olmadı
 };
 
 int main() {
